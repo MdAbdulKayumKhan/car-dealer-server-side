a5e6a9e61d7621f2d45ee57a995be246da4a1245
MdAbdulKayumKhan <kayum261@gmail.com>
2021-11-13 18:48:55 +0600

    manage order, products, delete, update status added

--- index.js
+++ index.js
@@ -42,8 +42,16 @@ async function run() {
       res.json(result);
     })
 
+    // DELETE API for products
+    app.delete('/manageProducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+    })
+
     // order single services: get
-    app.get('/order/:id', async (req, res)=>{
+    app.get('/order/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productsCollection.findOne(query);
@@ -52,50 +60,91 @@ async function run() {
     })
 
     // order service : post
-    app.post('/order', async (req, res)=>{
+    app.post('/order', async (req, res) => {
       const result = await ordersCollection.insertOne(req.body);
       console.log('order add ', result.insertedId)
       res.json(result)
     })
 
+    // order get all 
+    app.get('/orders', async (req, res) => {
+      const sort = { _id: -1 }
+      const cursor = ordersCollection.find({});
+      const result = await cursor.sort(sort).toArray();
+      res.json(result);
+    })
+   
+
+    // order get: by email
+    app.get('/orders/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = ordersCollection.find(query);
+      const result = await cursor.toArray();
+      res.json(result);
+
+    })
+    // update status
+    app.put('/updateStatus/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: 'Shipped'
+        },
+      };
+      const result = await ordersCollection.updateOne(filter, updateDoc)
+      console.log('status updated', id);
+      res.send(result);
+    })
+
+    // DELETE API for Manage Service
+    app.delete('/manageServices/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    })
+
 
     // users: post
-    app.post('/users', async (req, res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       console.log(result)
       res.json(result)
     })
-  
-    app.put('/users', async (req, res)=>{
+
+    app.put('/users', async (req, res) => {
       const user = req.body;
       console.log(user)
-      const filter = {email: user.email};
-      const options = {upsert: true};
-      const updateDoc = {$set: user};
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
       const result = await usersCollection.updateOne(filter, updateDoc, options);
       res.json(result);
     })
 
     // make admin 
-    app.put('/users/admin/', async (req, res)=>{
+    app.put('/users/admin/', async (req, res) => {
       const user = req.body;
       console.log('put', user)
-      const filter = {email: user.email};
-      const updateDoc = {$set: {role: 'admin'}};
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
     })
     // check admin
-    app.get('/users/:email', async (req, res)=>{
+    app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
-      const query = {email: email};
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
       let isAdmin = false;
-      if(user?.role === 'admin'){
+      if (user?.role === 'admin') {
         isAdmin = true;
       }
-      res.json({admin: isAdmin});
+      res.json({ admin: isAdmin });
     })
 
   } finally {
